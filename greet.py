a8d3585c6a57684b77d6c08b7ea413693d096883
PierreGode <pierre@gode.one>
2024-10-25 08:46:05 +0200

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,18 @@
+def greet(name)
+    print("Hello, " + name)
+    
+greet("Alice"
+
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(10, 5
+print("Result:", result)
+
+def divide_numbers(a, b):
+    if b == 0:
+        print("Cannot divide by zero!")
+    return a / b
+    
+result2 = divide_numbers(10, 0)
+print("Division Result:", result2)
